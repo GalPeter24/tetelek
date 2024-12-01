4523b58f413b4122bd423e0bf106e748636c93c6
GalPeter24 <galpeti20083e@gmail.com>
2024-12-01 15:38:12 +0100

    2.1_feledat_kesz

---
+++ szamlalas_2.1_feladat.py
@@ -0,0 +1,7 @@
+szavak = ['alma', 'barack', 'Attila', 'kávé', 'szekrény', 'asztal']
+darab = 0
+
+for szo in szavak :
+    if szo[0] =="a" or szo[0] =="A":
+        darab += 1 
+print( darab, ' darab "A" vagy "a" betűvel kezdődő szó van')
